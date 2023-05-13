d7bc7fe55f37c522fcbd2ad29609ed6c7810494e
MaxWe00 <gitlab.9v1lq@slmail.me>
2023-05-13 15:52:19 +0200

    Update args

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import click
 
 @click.command()
+@click.argument('image', type=click.types.Path(exists=True, dir_okay=False), required=True)
 @click.option('--threshold', type=click.types.IntRange(0, 255), help='Applies a threshold filter to the image.')
 @click.option('--brightness', type=click.types.IntRange(-255, 255), help='Modifies the brightness of the image.')
 @click.option('--contrast', type=click.types.IntRange(-255, 255), help='Modifies the contrast of the image.')
 @click.option('--blur', type=click.types.IntRange(0, 100), help='Applies a blur filter to the image.')
 @click.option('--sharpen', type=click.types.IntRange(0, 100), help='Applies a sharpen filter to the image.')
-def edit(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+def edit(image, threshold, brightness, contrast, blur, sharpen):
+    if threshold:
+        print("Threshold")
+    if brightness:
+        print("Brightness")
+    if contrast:
+        print("Contrast")
+    if blur:
+        print("Blur")
+    if sharpen:
+        print("Sharpen")
 
 if __name__ == '__main__':
     edit()
--- util.py
+++ util.py
@@ -1,4 +1,14 @@
 # Threshold
+def threshold(image, value):
+    img = image.copy()
+    for x in range(img.width):
+        for y in range(img.height):
+            if img.getpixel((x, y)) < value:
+                new_value = 0
+            else:
+                new_value = 255
+            img.putpixel((x, y), new_value)
+    return img
 
 # Brightness
 

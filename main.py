067c108883b9d4330f02afd2e2e66ce295b540ad
MaxWe00 <gitlab.9v1lq@slmail.me>
2023-05-13 15:42:04 +0200

    Help texts and sources

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import click
 
 @click.command()
-@click.option('--threshold', type=int, help='Threshold.')
-@click.option('--brightness', type=int, help='Threshold.')
-@click.option('--contrast', type=int, help='Threshold.')
-@click.option('--blur', type=int, help='Threshold.')
-@click.option('--sharpen', type=int, help='Threshold.')
+@click.option('--threshold', type=click.types.IntRange(0, 255), help='Applies a threshold filter to the image.')
+@click.option('--brightness', type=click.types.IntRange(-255, 255), help='Modifies the brightness of the image.')
+@click.option('--contrast', type=click.types.IntRange(-255, 255), help='Modifies the contrast of the image.')
+@click.option('--blur', type=click.types.IntRange(0, 100), help='Applies a blur filter to the image.')
+@click.option('--sharpen', type=click.types.IntRange(0, 100), help='Applies a sharpen filter to the image.')
 def edit(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):

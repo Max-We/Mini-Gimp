01a4ec94507637de74973935535d630b033268bc
MaxWe00 <gitlab.9v1lq@slmail.me>
2023-05-13 16:14:13 +0200

    Threshold, Brightness, Contrast

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import click
+from PIL import Image
+
+from util import apply_threshold, apply_brightness, apply_contrast
+
 
 @click.command()
-@click.argument('image', type=click.types.Path(exists=True, dir_okay=False), required=True)
+@click.argument('image-path', type=click.types.Path(exists=True, dir_okay=False), required=True)
 @click.option('--threshold', type=click.types.IntRange(0, 255), help='Applies a threshold filter to the image.')
 @click.option('--brightness', type=click.types.IntRange(-255, 255), help='Modifies the brightness of the image.')
 @click.option('--contrast', type=click.types.IntRange(-255, 255), help='Modifies the contrast of the image.')
 @click.option('--blur', type=click.types.IntRange(0, 100), help='Applies a blur filter to the image.')
 @click.option('--sharpen', type=click.types.IntRange(0, 100), help='Applies a sharpen filter to the image.')
-def edit(image, threshold, brightness, contrast, blur, sharpen):
+def edit(image_path, threshold, brightness, contrast, blur, sharpen):
+    image = Image.open(image_path)
+
     if threshold:
-        print("Threshold")
+        image = apply_threshold(image, threshold)
     if brightness:
-        print("Brightness")
+        image = apply_brightness(image, brightness)
     if contrast:
-        print("Contrast")
+        image = apply_contrast(image, contrast)
     if blur:
         print("Blur")
     if sharpen:
         print("Sharpen")
+
+    image.save("example_out.png")
 
 if __name__ == '__main__':
     edit()
--- util.py
+++ util.py
@@ -1,6 +1,9 @@
+from PIL import Image
+
+
 # Threshold
-def threshold(image, value):
-    img = image.copy()
+def apply_threshold(image: Image, value: int) -> Image:
+    img = image.convert("L").copy()
     for x in range(img.width):
         for y in range(img.height):
             if img.getpixel((x, y)) < value:
@@ -8,11 +11,31 @@ def threshold(image, value):
             else:
                 new_value = 255
             img.putpixel((x, y), new_value)
+
     return img
 
 # Brightness
+def apply_brightness(image: Image, value: int) -> Image:
+    img = image.copy()
+    for x in range(img.width):
+        for y in range(img.height):
+            channels = list(img.getpixel((x, y)))
+            img.putpixel((x, y), tuple([c + value for c in channels]))
+
+    return img
 
 # Contrast
+# https://www.perplexity.ai/search/8a122732-bffb-4b6f-a0db-18c5b3555a18?s=t
+def apply_contrast(image, value):
+    img = image.copy()
+    factor = (259 * (value + 255)) / (255 * (259 - value))
+    for x in range(img.width):
+        for y in range(img.height):
+            channels = list(img.getpixel((x, y)))
+            channels = [round(factor * (c - 128) + 128) for c in channels]
+            img.putpixel((x, y), tuple(channels))
+
+    return img
 
 # Blur
 
